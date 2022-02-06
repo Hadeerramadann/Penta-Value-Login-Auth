e71ad7589eb9fe1502185a6b561afb4f466eae7b
ahmed <ahmedh.mohamed1995@gmail.com>
2022-02-05 16:13:23 -0800

    login with phone

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>pentavalue Phone Number OTP Auth task</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container mt-5" style="max-width: 550px">
+        <div class="alert alert-danger" id="error" style="display: none;"></div>
+        <h3>Add Phone Number</h3>
+        <div class="alert alert-success" id="successAuth" style="display: none;"></div>
+        <form>
+            <label>Phone Number:</label>
+            <input type="text" id="number" class="form-control" placeholder="+91 ********">
+            <div id="recaptcha-container"></div>
+            <button type="button" class="btn btn-primary mt-3" onclick="sendOTP();">Send OTP</button>
+        </form>
+
+        <div class="mb-5 mt-5">
+            <h3>Add verification code</h3>
+            <div class="alert alert-success" id="successOtpAuth" style="display: none;"></div>
+            <form>
+                <input type="text" id="verification" class="form-control" placeholder="Verification code">
+                <button type="button" class="btn btn-danger mt-3" onclick="verify()">Verify code</button>
+            </form>
+        </div>
+    </div>
+    
+</body> <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- Firebase App (the core Firebase SDK) is always required and must be listed first -->
+    <script src="https://www.gstatic.com/firebasejs/6.0.2/firebase.js"></script>
+
+    <script>
+        var firebaseConfig = {
+            apiKey: "AIzaSyBNROqfWP9TEg4nu_urVJVXtFy63p_FFIE",
+            authDomain: "loginfireauth-d9a12.firebaseapp.com",
+            projectId: "loginfireauth-d9a12",
+            storageBucket: "loginfireauth-d9a12.appspot.com",
+            messagingSenderId: "805544357628",
+            appId: "1:805544357628:web:5fe4b7f31428e6ea9a7179",
+            measurementId: "G-3B2XYRK3XS"
+        };
+        firebase.initializeApp(firebaseConfig);
+    </script>
+    <script type="text/javascript">
+        window.onload = function () {
+            render();
+        };
+        function render() {
+            window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+            recaptchaVerifier.render();
+        }
+        function sendOTP() {
+            var number = $("#number").val();
+            firebase.auth().signInWithPhoneNumber(number, window.recaptchaVerifier).then(function (confirmationResult) {
+                window.confirmationResult = confirmationResult;
+                coderesult = confirmationResult;
+                console.log(coderesult);
+                $("#successAuth").text("Message sent");
+                $("#successAuth").show();
+            }).catch(function (error) {
+                $("#error").text(error.message);
+                $("#error").show();
+            });
+        }
+        function verify() {
+            var code = $("#verification").val();
+            coderesult.confirm(code).then(function (result) {
+                var user = result.user;
+                console.log(user);
+                $("#successOtpAuth").text("Auth is successful");
+                $("#successOtpAuth").show();
+            }).catch(function (error) {
+                $("#error").text(error.message);
+                $("#error").show();
+            });
+        }
+    </script>
+</html>
